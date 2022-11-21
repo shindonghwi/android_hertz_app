1ab9deb6443def7fc60cac02bc7139305409ec35
shindonghwi <ehdgnl8940@naver.com>
2022-11-21 18:54:36 +0900

    gitignore 다시 적용

--- presentation/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_mago_apps_hertz_MagoHertzApp.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "mago.apps.hertz.MagoHertzApp"
-)
-public final class _mago_apps_hertz_MagoHertzApp {
-}
