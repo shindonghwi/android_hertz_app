b03b40dfda3e1823c03c31e05d326a1d5685f8b9
shindonghwi <ehdgnl8940@naver.com>
2022-11-21 18:55:34 +0900

    style: color 추가 ( 다크, 라이트 )

--- presentation/src/main/java/mago/apps/hertz/ui/theme/Color.kt
+++ presentation/src/main/java/mago/apps/hertz/ui/theme/Color.kt
@@ -2,7 +2,45 @@ package mago.apps.hertz.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+val Primary99 = Color(0xFFFFFAFF)
+val Primary95 = Color(0xFFECEAFF)
+val Primary90 = Color(0xFFD7D8FF)
+val Primary80 = Color(0xFFADB3FF)
+val Primary70 = Color(0xFF8491FF)
+val Primary60 = Color(0xFF5D6DFF)
+val Primary50 = Color(0xFF4151E4)
+val Primary40 = Color(0xFF2A38C4)
+val Primary30 = Color(0xFF121FA8)
+val Primary20 = Color(0xFF01107A)
+val Primary10 = Color(0xFF010947)
+
+val Secondary99 = Color(0xFFFFFAFF)
+val Secondary95 = Color(0xFFEDEAFF)
+val Secondary90 = Color(0xFFD7D9F7)
+val Secondary80 = Color(0xFFB6B7D5)
+val Secondary70 = Color(0xFF9799B4)
+val Secondary60 = Color(0xFF7A7C95)
+val Secondary50 = Color(0xFF606278)
+val Secondary40 = Color(0xFF494A5F)
+val Secondary30 = Color(0xFF343547)
+val Secondary20 = Color(0xFF222333)
+val Secondary10 = Color(0xFF131421)
+
+val Error99 = Color(0xFFFFFAFF)
+val Error95 = Color(0xFFFEE8E5)
+val Error80 = Color(0xFFFDA39B)
+val Error70 = Color(0xFFFC736A)
+val Error60 = Color(0xFFFC3B39)
+val Error50 = Color(0xFFD52025)
+val Error40 = Color(0xFFAB0215)
+val Error30 = Color(0xFF80000B)
+val Error20 = Color(0xFF560007)
+val Error10 = Color(0xFF310003)
+
+
+val Neutral99 = Color(0xFFFFFAFF)
+val Neutral90 = Color(0xFFDDDAE0)
+val Neutral10 = Color(0xFF151518)
 
 val White = Color(0xFFFFFFFF)
 val Black = Color(0xFF000000)
-val Primary = Color(0xFFFF5D39)
--- presentation/src/main/java/mago/apps/hertz/ui/theme/Theme.kt
+++ presentation/src/main/java/mago/apps/hertz/ui/theme/Theme.kt
@@ -5,17 +5,36 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
-
-private val DarkColorPalette = darkColors(
-    primary = Primary,
-    primaryVariant = Primary,
-    secondary = Primary
-)
+import androidx.compose.ui.graphics.Color
 
 private val LightColorPalette = lightColors(
-    primary = Primary,
-    primaryVariant = Primary,
-    secondary = Primary
+    primary = Primary40, // 기본 색상은 앱에서 가장 자주 표시되는 색상입니다.
+    primaryVariant = Primary90, // 기본 변형 색상은 두 요소를 구별하는 데 사용됩니다.
+    onPrimary = White, // 주색 위에 표시되는 텍스트 및 아이콘에 사용되는 색상입니다.
+    secondary = Secondary40, // 두 번째 색상은 당신을 강조하고 구별할 수 있는 더 많은 방법을 제공합니다.
+    secondaryVariant = Secondary90, // 보조 변형 색상은 의 두 가지 요소를 구별하는 데 사용됩니다.
+    onSecondary = White, // 보조 색상 위에 표시되는 텍스트 및 아이콘에 사용되는 색상입니다.
+    background = Neutral99, // 스크롤 가능한 콘텐츠 뒤에 배경색이 나타납니다.
+    onBackground = Neutral10, // 배경색 위에 표시되는 텍스트 및 아이콘에 사용되는 색상입니다.
+    surface = Neutral99, // 표면 색상은 카드, 시트 및 같은 구성 요소의 표면에 사용됩니다.
+    onSurface = Neutral10, // 표면 색상 위에 표시되는 텍스트 및 아이콘에 사용됩니다.
+    error = Error40, // 오류 색상은 텍스트 필드와 같은 구성 요소 내의 오류를 나타내는 데 사용됩니다.
+    onError = White, // 오류 색상 위에 표시되는 텍스트 및 아이콘에 사용됩니다.
+)
+
+private val DarkColorPalette = darkColors(
+    primary = Primary80, // 기본 색상은 앱에서 가장 자주 표시되는 색상입니다.
+    primaryVariant = Primary30, // 기본 변형 색상은 두 요소를 구별하는 데 사용됩니다.
+    onPrimary = Primary20, // 주색 위에 표시되는 텍스트 및 아이콘에 사용되는 색상입니다.
+    secondary = Secondary80, // 두 번째 색상은 당신을 강조하고 구별할 수 있는 더 많은 방법을 제공합니다.
+    secondaryVariant = Secondary30, // 보조 변형 색상은 의 두 가지 요소를 구별하는 데 사용됩니다.
+    onSecondary = Secondary20, // 보조 색상 위에 표시되는 텍스트 및 아이콘에 사용되는 색상입니다.
+    background = Neutral10, // 스크롤 가능한 콘텐츠 뒤에 배경색이 나타납니다.
+    onBackground = Neutral90, // 배경색 위에 표시되는 텍스트 및 아이콘에 사용되는 색상입니다.
+    surface = Neutral10, // 표면 색상은 카드, 시트 및 같은 구성 요소의 표면에 사용됩니다.
+    onSurface = Neutral90, // 표면 색상 위에 표시되는 텍스트 및 아이콘에 사용됩니다.
+    error = Error80, // 오류 색상은 텍스트 필드와 같은 구성 요소 내의 오류를 나타내는 데 사용됩니다.
+    onError = Error20, // 오류 색상 위에 표시되는 텍스트 및 아이콘에 사용됩니다.
 )
 
 @Composable
