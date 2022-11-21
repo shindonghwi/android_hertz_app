8a6e63dcd49f78c44f97cb5781e43f82d682f654
shindonghwi <ehdgnl8940@naver.com>
2022-11-21 17:48:52 +0900

    gitignore 다시 적용

---
+++ presentation/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_mago_apps_hertz_MagoHertzApp.java
@@ -0,0 +1,9 @@
+package dagger.hilt.internal.processedrootsentinel.codegen;
+
+import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
+
+@ProcessedRootSentinel(
+    roots = "mago.apps.hertz.MagoHertzApp"
+)
+public final class _mago_apps_hertz_MagoHertzApp {
+}
---
+++ presentation/build/generated/source/kapt/debug/mago/apps/hertz/MagoHertzApp_GeneratedInjector.java
@@ -0,0 +1,17 @@
+package mago.apps.hertz;
+
+import dagger.hilt.InstallIn;
+import dagger.hilt.codegen.OriginatingElement;
+import dagger.hilt.components.SingletonComponent;
+import dagger.hilt.internal.GeneratedEntryPoint;
+import javax.annotation.processing.Generated;
+
+@OriginatingElement(
+    topLevelClass = MagoHertzApp.class
+)
+@GeneratedEntryPoint
+@InstallIn(SingletonComponent.class)
+@Generated("dagger.hilt.android.processor.internal.androidentrypoint.InjectorEntryPointGenerator")
+public interface MagoHertzApp_GeneratedInjector {
+  void injectMagoHertzApp(MagoHertzApp magoHertzApp);
+}
