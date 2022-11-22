02fa3e88b66b95b345674531eb7b637f0f1f5f5e
shindonghwi <ehdgnl8940@naver.com>
2022-11-22 10:33:18 +0900

    feat: extension 추가

---
+++ presentation/src/main/java/mago/apps/hertz/ui/utils/scope/CoroutineScope.kt
@@ -0,0 +1,29 @@
+package mago.apps.hertz.ui.utils.scope
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+inline fun coroutineScopeOnMain(
+    initDelay: Long = 0,
+    crossinline body: suspend CoroutineScope.() -> Unit
+) = CoroutineScope(Dispatchers.Main).launch {
+    delay(initDelay)
+    body()
+}
+
+inline fun coroutineScopeOnIO(
+    initDelay: Long = 0,
+    crossinline body: suspend CoroutineScope.() -> Unit
+) = CoroutineScope(Dispatchers.IO).launch {
+    delay(initDelay)
+    body()
+}
+
+inline fun coroutineScopeOnDefault(
+    initDelay: Long = 0,
+    crossinline body: suspend CoroutineScope.() -> Unit,
+) = CoroutineScope(Dispatchers.Default).launch {
+    delay(initDelay)
+    body()
+}
