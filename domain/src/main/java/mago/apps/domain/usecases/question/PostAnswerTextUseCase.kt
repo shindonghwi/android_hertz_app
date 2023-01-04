b0add568744ed23a7e6dcd3448aa1cd06b40e768
shindonghwi <ehdgnl8940@naver.com>
2023-01-04 17:07:49 +0900

    fix: 사용하지 않는기능 제거

--- domain/src/main/java/mago/apps/domain/usecases/question/PostAnswerTextUseCase.kt
+++ domain/src/main/java/mago/apps/domain/usecases/question/PostAnswerTextUseCase.kt
@@ -2,7 +2,6 @@ package mago.apps.domain.usecases.question
 
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
-import mago.apps.domain.constants.NOT_ALLOWED_PARAMS
 import mago.apps.domain.model.answer.Answer
 import mago.apps.domain.model.common.EmotionType
 import mago.apps.domain.model.common.Resource
