c5969660642d4542925c8a4b202fe0beda681a63
shindonghwi <ehdgnl8940@naver.com>
2023-01-25 17:28:10 +0900

    feat: 디버그 모드 설정

--- data/src/main/java/mago/apps/data/constants/Constants.kt
+++ data/src/main/java/mago/apps/data/constants/Constants.kt
@@ -1,6 +1,13 @@
 package mago.apps.data.constants
 
-const val BASE_URL = "https://hertz-api-dev.mago52.com"
+import mago.apps.data.BuildConfig
+
+val BASE_URL = if (BuildConfig.DEBUG) {
+    "https://hertz-api-dev.mago52.com"
+} else {
+    "https://hertz-api-dev.mago52.com"
+}
+
 const val API_VERSION = "v1"
 const val HEADER_KEY = "Application-Client-ID"
 const val HEADER_VALUE = "hertz-android-1"
