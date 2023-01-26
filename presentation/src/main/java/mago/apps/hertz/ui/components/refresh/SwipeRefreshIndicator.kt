346336d573ae113a657bf2fd21227984d2ea33ca
shindonghwi <ehdgnl8940@naver.com>
2023-01-26 14:45:24 +0900

    chore: 0.0.1 ( 1 ) firebase 테스터 배포 (마고)

--- presentation/src/main/java/mago/apps/hertz/ui/components/refresh/SwipeRefreshIndicator.kt
+++ presentation/src/main/java/mago/apps/hertz/ui/components/refresh/SwipeRefreshIndicator.kt
@@ -15,7 +15,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.rotate
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
 @OptIn(ExperimentalMaterialApi::class)
